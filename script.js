d566fa444e24159036967245b5e6351340d56f80
AAAAgnes <mudshake1127@gmail.com>
2024-05-22 13:26:32 +0900

    fetch test

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", function() {
     window.location.href = pageURL;
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Load header
+  fetch('header.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('header-container').innerHTML = data;
+      });
+
+  // Load footer
+  fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('footer-container').innerHTML = data;
+      });
+});
