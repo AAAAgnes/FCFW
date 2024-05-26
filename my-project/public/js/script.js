4bdd96c9bc6c6c5bb4f640392ae8ef2995eb653d
AAAAgnes <mudshake1127@gmail.com>
2024-05-27 04:07:07 +0900

    imagetest

--- my-project/public/js/script.js
+++ my-project/public/js/script.js
@@ -12,19 +12,3 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Load header
-  fetch('header.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('header-container').innerHTML = data;
-      });
-
-  // Load footer
-  fetch('footer.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('footer-container').innerHTML = data;
-      });
-});
