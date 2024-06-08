58455d1fe81f3a007f586895204cb4542cc5f321
AAAAgnes <mudshake1127@gmail.com>
2024-06-08 23:47:09 +0900

    f

--- my-project/server.js
+++ my-project/server.js
@@ -6,20 +6,32 @@ const port = 3000;
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 뷰 엔진 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // 'views' 디렉토리 경로 설정
+
 // Route for the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('index'); // 'views/index.ejs' 파일을 렌더링
 });
 
 // 라우트 설정
 app.get('/luck', (req, res) => {
   const imageNumber = req.query.image;
-  res.render('luck', { imageNumber: imageNumber });
+  res.render('luck', { imageNumber }); // 'views/luck.ejs' 파일을 렌더링하고 imageNumber 변수를 전달
+});
+
+// 라우트 설정
+app.get('/event', (req, res) => {
+  res.render('event'); // 'views/event.ejs' 파일을 렌더링
+});
+
+// 라우트 설정
+app.get('/info', (req, res) => {
+  res.render('info'); // 'views/event.ejs' 파일을 렌더링
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-    
-   
